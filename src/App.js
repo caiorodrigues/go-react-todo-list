6d213e8045884fdc693291c20440540d26af5460
Caio Rodrigues <caiorodrigues@hotmail.com>
2019-05-14 16:24:30 -0300

    Todo List - Reactotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { Provider } from "react-redux";
+import React from 'react';
+import './config/ReactotronConfig';
 
-import store from "./store";
+import { Provider } from 'react-redux';
 
-import TodoList from "./TodoList";
+import store from './store';
+
+import TodoList from './TodoList';
+
+console.tron.log({ hello: 'World' });
 
 const App = () => (
   <Provider store={store}>
